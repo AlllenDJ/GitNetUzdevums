6963141e960b93e57b26ead197ada3374c26cb6a
AlllenDJ <alenisn29@gmail.com>
2025-05-10 03:14:01 +0500

    New Fork Changes

--- GitNetUzdevums/Form1.cs
+++ GitNetUzdevums/Form1.cs
@@ -77,5 +77,35 @@ namespace WindowsFormsApplication_15
                 x += step;
             }
         }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double m = 30; // mērogs
+            int xc = pictureBox1.Width / 2;
+            int yc = pictureBox1.Height / 2;
+            int xe, ye;
+            double x, y;
+            double step = 0.005;
+            Graphics G = pictureBox1.CreateGraphics();
+            G.Clear(System.Drawing.Color.White);
+            Pen myPen = new Pen(Color.Silver);
+            G.DrawLine(myPen, 10, yc, 2 * xc - 10, yc);   // x-ass
+            G.DrawLine(myPen, xc, 10, xc, 2 * yc - 10);   // y-ass
+            myPen = new Pen(Color.Blue);                 // different color
+            x = -Math.PI;
+            while (x < Math.PI)
+            {
+                try
+                {
+                    y = Math.Cos(x);
+                    xe = (int)(xc + m * x);
+                    ye = (int)(yc - m * y);
+                    G.DrawEllipse(myPen, xe, ye, 1, 1);
+                }
+                catch { }
+                x += step;
+            }
+        }
     }
 }
